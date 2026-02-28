3e21e63fe2afe9ec8467af33a1cf378bf73a7346
Shunmuga Sundaram <shunmugasundaram003@email.com>
2026-02-28 18:30:06 +0530

    Very Beginner friendly

---
+++ src/test/java/com/example/Movie_Review_BackendApp/MovieReviewBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Movie_Review_BackendApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewBackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
